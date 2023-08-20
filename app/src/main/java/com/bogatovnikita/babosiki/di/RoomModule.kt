dcb8b3112be973756e558714c29ffe4555f3e25f
Nikita Bogatov <bogatovnikita@gmail.com>
2023-08-20 14:51:58 +0400

    add di for room

---
+++ app/src/main/java/com/bogatovnikita/babosiki/di/RoomModule.kt
@@ -0,0 +1,33 @@
+package com.bogatovnikita.babosiki.di
+
+import android.content.Context
+import androidx.room.Room
+import com.bogatovnikita.babosiki.data.room.ExchangeRatesDao
+import com.bogatovnikita.babosiki.data.room.ExchangeRatesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideExchangeRatesDatabase(@ApplicationContext context: Context): ExchangeRatesDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            ExchangeRatesDatabase::class.java,
+            "exchange_rates_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideExchangeRatesDao(database: ExchangeRatesDatabase): ExchangeRatesDao {
+        return database.exchangeRatesDao()
+    }
+}
