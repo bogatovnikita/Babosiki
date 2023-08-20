2ac7228b0d96c26edd114e1d776c2eeecffaa2da
Nikita Bogatov <bogatovnikita@gmail.com>
2023-08-20 14:53:59 +0400

    connected hilt

--- app/src/main/java/com/bogatovnikita/babosiki/host/MainActivity.kt
+++ app/src/main/java/com/bogatovnikita/babosiki/host/MainActivity.kt
@@ -3,7 +3,9 @@ package com.bogatovnikita.babosiki.host
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bogatovnikita.babosiki.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
