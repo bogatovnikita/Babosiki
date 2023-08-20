fccabd179336e0f8cd9c2ac37dae2f12d34fd3b3
Nikita Bogatov <bogatovnikita@gmail.com>
2023-08-20 14:54:26 +0400

    got the result from retrofit and recorded it in room

--- data/src/main/java/com/bogatovnikita/babosiki/data/repository/PopularRepositoryImplementation.kt
+++ data/src/main/java/com/bogatovnikita/babosiki/data/repository/PopularRepositoryImplementation.kt
@@ -1,16 +1,45 @@
 package com.bogatovnikita.babosiki.data.repository
 
 import com.bogatovnikita.babosiki.data.retrofit.ApiService
+import com.bogatovnikita.babosiki.data.room.ExchangeRatesDao
+import com.bogatovnikita.babosiki.data.room.ExchangeRatesEntity
 import com.bogatovnikita.babosiki.domain.models.ExchangeRate
 import com.bogatovnikita.babosiki.domain.repositoryies.PopularRepository
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import retrofit2.awaitResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class PopularRepositoryImplementation @Inject constructor(private val apiService: ApiService) :
-    PopularRepository {
-    override fun getAllCurrencies(): StateFlow<ExchangeRate> {
-        TODO("Not yet implemented")
+class PopularRepositoryImplementation @Inject constructor(
+    private val apiService: ApiService,
+    private val exchangeRatesDao: ExchangeRatesDao
+) : PopularRepository {
+    override suspend fun getAllCurrencies(baseCurrency: String): Flow<ExchangeRate> {
+        val response = apiService.getExchangeRates(baseCurrency).awaitResponse()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            exchangeRatesDao.insertExchangeRates(
+                ExchangeRatesEntity(
+                    baseCurrency = body.baseCurrency,
+                    date = body.date,
+                    rates = body.rates,
+                    success = body.success,
+                    timestamp = body.timestamp
+                )
+            )
+        } else {
+            throw IllegalAccessException("Bad response")
+        }
+        return exchangeRatesDao.getLatestExchangeRatesFlow().map {
+            ExchangeRate(
+                baseCurrency = it.baseCurrency ?: "",
+                date = it.date,
+                rates = it.rates,
+                success = it.success,
+                timestamp = it.timestamp
+            )
+        }
     }
 }
